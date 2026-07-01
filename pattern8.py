7915e0d195cccd161e3ea8bd3e2cea3a98684c1f
Nandhana_gopan0505 <gopannandhana727@gmail.com>
2026-07-01 21:34:07 +0530

    day2

---
+++ pattern8.py
@@ -0,0 +1,12 @@
+for i in range(1,6):
+    for j in range(5-i):
+        print(" ",end="")
+    for k in range(2*i-1):
+        print("*",end="")
+    print()
+for x in range(4,0,-1):
+    for y in range(5-x):
+        print(" ",end="")
+    for z in range(2*x-1):
+        print("*",end="")
+    print()
